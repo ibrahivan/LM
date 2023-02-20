25bfb905f7098961c0ba652ebf44dcc89e1186cf
DESKTOP-PUUUBQI\ivan vazquez <ivan.vazquez.cod@gmail.com>
2023-02-20 09:57:49 +0100

    no message

--- edu.ejercicios/src/main/webapp/script/codEj7.js
+++ edu.ejercicios/src/main/webapp/script/codEj7.js
@@ -1,7 +1,7 @@
 function ej7(){
 	let n=prompt("Indica un numero");
 	if (n%2==0 || n==0)
-		document.write("El numero es par");
+		document.write("El  numero "+n+" es par");
 	else
-		document.write("El numero es impar");
+		document.write("El numero "+n+" es impar");
 }
--- edu.ejercicios/src/main/webapp/script/codEj8.js
+++ edu.ejercicios/src/main/webapp/script/codEj8.js
@@ -1,10 +1,10 @@
 function ej8(){
 	let cadena=prompt("Escribe una frase con mayúsculas, minúsculas o ambas");
 	if (cadena==cadena.toUpperCase())
-		document.write("La cadena es formada por mayúsculas")
+		document.write("La cadena es formada por mayúsculas");
 	else if (cadena==cadena.toLowerCase())
-		document.write("La cadena es formada por minúsculas")
+		document.write("La cadena es formada por minúsculas");
 	else
-		document.write("La cadena es formada por mayúsculas y minúsculas")
+		document.write("La cadena es formada por mayúsculas y minúsculas");
 
 }
