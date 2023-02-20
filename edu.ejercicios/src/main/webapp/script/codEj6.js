b354836b59f8f963417edb759c6f9e1517300731
DESKTOP-PUUUBQI\ivan vazquez <ivan.vazquez.cod@gmail.com>
2023-02-20 10:00:06 +0100

    no message

--- edu.ejercicios/src/main/webapp/script/codEj6.js
+++ edu.ejercicios/src/main/webapp/script/codEj6.js
@@ -1,7 +1,7 @@
 function ej6(){
 	let n =prompt("Indique numero para calcularle el factorial");
 	let factorial=1;
-	for(let i =2; i<=n ;i++)
+	for(let i =1; i<=n ;i++)
 		 factorial=factorial*i;
 	document.write("El factorial es "+n+"!="+factorial);
 }
