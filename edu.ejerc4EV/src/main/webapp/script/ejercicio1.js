34fdc5afc71709fc11a3fa33697cd59c1f50644c
DESKTOP-PUUUBQI\ivan vazquez <ivan.vazquez.cod@gmail.com>
2023-03-15 10:06:21 +0100

    ejercicios 4EV

---
+++ edu.ejerc4EV/src/main/webapp/script/ejercicio1.js
@@ -0,0 +1,16 @@
+
+function ej1(){
+	class Segmento{
+		constructor (a,b){
+			this.a=prompt("Indique cual es el punto inicial del segmento");
+			this.b=prompt("Indique cual es el punto final del segmento");
+		}
+		calculoLongSeg(){
+			return this.b-this.a;
+		}
+
+	}
+
+	let Seg=new Segmento();
+	console.log("La distancia del segmento es de "+Seg.calculoLongSeg()+" cm.");
+}
