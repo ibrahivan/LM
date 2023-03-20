d9d9d1b9cae0127ff035cbb229e24480ca7f03eb
DESKTOP-PUUUBQI\ivan vazquez <ivan.vazquez.cod@gmail.com>
2023-03-20 13:33:40 +0100

    no message

---
+++ edu.examenED-LM/src/main/webapp/script/ClasesyFunciones.js
@@ -0,0 +1,100 @@
+function Menu() {
+	var listaAlumnos = [];
+
+	let opcion;
+	do {
+		opcion = prompt("[Prestamos]\n1. Matrícula de alumno\n2. Borrar alumno\n3. Listar alumnos\n0. Salir");
+
+		while (opcion != "0" && opcion != "1" && opcion != "2" && opcion != "3") {
+			alert("[Error] Introduzca una opción válida: ");
+			opcion = prompt("[Prestamos]\n1. Matrícula de alumno\n2. Borrar alumno\n3. Listar alumnos\n0. Salir");
+		}
+
+		switch (opcion) {
+			case "1":
+				AlumnoPort.añadirAlumno(listaAlumnos);
+				break;
+			case "2":
+				AlumnoPort.eliminarAlumno(listaAlumnos);
+				break;
+			case "3":
+				AlumnoPort.listarAlumnos(listaAlumnos);
+				break;
+		}
+	} while (opcion != "0");
+}
+
+class AlumnoPort {
+	constructor(idAlum,nombre, apellidos, telefono, idPortatil,marca,modelo) {
+		this.idAlum = idAlum;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.telefono = telefono;
+		this.idPortatil = idPortatil;
+		this.marca = marca;
+		this.modelo = modelo;
+	}
+	
+	static calculoIdAlumno(listaAlumnos) {
+		let auxiliar = 0;
+		for(let i = 0; i< listaAlumnos.length; i++) {
+			let j = listaAlumnos[i].idAlum;
+			if(auxiliar<j) {
+				auxiliar = j;
+			}
+		}
+		return auxiliar + 1;
+	}
+	
+	static concatenacionMarcaModelo( marca,  modelo) {
+		let idPortatil;
+		if(marca.length<3) {
+			idPortatil = marca;
+		}else {
+			idPortatil = marca.substring(0,3);
+		}
+		idPortatil += "-";
+		if(marca.length<3) {
+			idPortatil += modelo;
+		}else {
+			idPortatil += modelo.substring(0,3);
+		}
+		return idPortatil;
+	}
+	
+	static añadirAlumno(listaAlumnos) {
+		
+		let nombre = prompt("Introduzca el nombre: ");
+		let apellidos = prompt("Introduzca los apellidos: ");
+		let telefono = prompt("Introduzca el teléfono: ");
+		let marca = prompt("Introduzca la marca del portátil: ");
+		let modelo = prompt("Introduzca el modelo del portátil: ");
+		let idAlum= this.calculoIdAlumno(listaAlumnos);
+		let idPortatil= this.concatenacionMarcaModelo(marca,modelo);
+		//Creo en objeto con los nuevos parametros
+		var alumport = new AlumnoPort(nombre,apellidos,telefono,marca,modelo,idAlum,idPortatil);
+		//le paso a la lista el objeto
+		alumport.idAlum= listaAlumnos.length + 1;
+		listaAlumnos.push(alumport);
+	}
+
+	static eliminarAlumno(listaAlumnos) {
+		let idBorrar = prompt("Introduzca el id del alumno para borrar su información: ");
+
+		if (idBorrar > listaAlumnos.length || idBorrar < 1 || listaAlumnos.length == 0)
+			alert("[Error] No se ha podido borrar la información.");
+		else {
+			listaAlumnos.splice(idBorrar - 1, 1);
+			alert("La información se ha borrado correctamente.");
+		}
+	}
+
+	static listarAlumnos(listaAlumnos) {
+		if (listaAlumnos.length != 0)
+			for (let i = 0; i < listaAlumnos.length; i++)
+				alert(JSON.stringify(listaAlumnos[i]));
+		else
+			alert("[Error] No existe información para listar.");
+	}
+}
+
