b81d5f3872bc93ba1d5e8d1bda8a4b65307f159c
DESKTOP-PUUUBQI\ivan vazquez <ivan.vazquez.cod@gmail.com>
2023-04-10 08:42:05 +0200

    no message

---
+++ edu.ejerc4EV/src/main/webapp/script/ejercicio7.js
@@ -0,0 +1,5 @@
+function porciento(){
+    let valor = document.getElementsByName('valor')[0].value;
+    document.write("El 30% de "+ valor +" es: "+ valor*0.3);
+    console.log("El 30% de "+ valor +" es: "+valor*0.3);
+}
