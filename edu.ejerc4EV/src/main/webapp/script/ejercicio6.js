90948cda600f12f176211ee6de725ed48cd281ec
DESKTOP-PUUUBQI\ivan vazquez <ivan.vazquez.cod@gmail.com>
2023-04-10 08:54:04 +0200

    no message

--- edu.ejerc4EV/src/main/webapp/script/ejercicio6.js
+++ edu.ejerc4EV/src/main/webapp/script/ejercicio6.js
@@ -2,3 +2,9 @@ function guardar(){
     let mensaje = document.getElementsByName('tuMensaje')[0].value;
     document.write("Se guarda el mensaje " + mensaje);
 }
+
+function porciento(){
+    let valor = document.getElementsByName('valor')[0].value;
+    document.write("El 30% de "+ valor +" es: "+ valor*0.3);
+    console.log("El 30% de "+ valor +" es: "+valor*0.3);
+}
--- edu.ejerc4EV/src/main/webapp/script/ejercicio7.js
+++
@@ -1,5 +0,0 @@
-function porciento(){
-    let valor = document.getElementsByName('valor')[0].value;
-    document.write("El 30% de "+ valor +" es: "+ valor*0.3);
-    console.log("El 30% de "+ valor +" es: "+valor*0.3);
-}
